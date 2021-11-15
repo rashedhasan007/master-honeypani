ebec8a4da8fd7f630aada12f57e81a9727d0878c
rashedhasan007 <rashedhasanai@gmail.com>
2021-11-15 13:29:19 +0600

    Initial commit

---
+++ clientdashboard/urls.py
@@ -0,0 +1,27 @@
+"""clientdashboard URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from .views import index,product,cart,myindex
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$',index,name='index'),
+    url('product/',product,name='product'),
+    url('cart/',cart,name='cart'),
+    path('myindex/<my_id>/',myindex, name='myindex'),
+
+]
---
+++ clientdashboard/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404, redirect
+from django.template.context import RequestContext
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from firebase import firebase
+
+def hello(request):
+   text = """<h1>welcome to my app !</h1>"""
+   return HttpResponse(text)
+
+def index(request):
+    from firebase import firebase
+    firebase = firebase.FirebaseApplication('https://honeypani-360ce-default-rtdb.firebaseio.com/', None)
+    result = firebase.get('batters', '')
+    print(result)
+    return render(request,'index.html',result)
+def product(request):
+   time_series_json="my name is rashed"
+   context={}
+   context['Shop_name']="hey welcome to my store"
+   return render(request,'product-detail.html',context)
+def cart(request):
+
+    b = """{u'1': u'xxxxx', u'2': u'yyyyy'}"""
+    a = json.loads(b)
+    return render(request,'cart.html',a)
+
+
+def myindex(request,my_id):
+    # initializations
+    cred = credentials.Certificate('try.json')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    # Reading the data
+
+    emp_ref = db.collection(my_id)
+    docs = emp_ref.stream()
+    print(docs)
+
+    for doc in docs:
+        print('{} => {} '.format(doc.id, doc.to_dict()))
+
+    return render(request,'index.html')
